236823aa1761de5d0f1418a799a9280723967a9b
Sebastian Salas <sebastiansalasrojas7@gmail.com>
2026-08-18 12:20:55 -0500

    feat(ux): transicion de entrada al cambiar entre home y contacto

--- src/app/(portfolio)/contact/page.tsx
+++ src/app/(portfolio)/contact/page.tsx
@@ -1,19 +1,22 @@
 import { Feedback } from "./ui/Feedback";
 import { InfoContact } from "./ui/InfoContact";
+import { PageTransition } from "@/components/page-transition/PageTransition";
 
 export default function ContactPage() {
   return (
-    <section
-      className="bg-[#101010] max-w-[1580px]
-        md:flex md:flex-col md:items-center md:justify-center
-        lg:flex-row lg:items-center"
-    >
-      <div className="lg:basis-[60%] ">
-        <Feedback />
-      </div>
-      <div className="lg:basis-[40%] lg:order-first">
-        <InfoContact />
-      </div>
-    </section>
+    <PageTransition>
+      <section
+        className="bg-[#101010] max-w-[1580px]
+          md:flex md:flex-col md:items-center md:justify-center
+          lg:flex-row lg:items-center"
+      >
+        <div className="lg:basis-[60%] ">
+          <Feedback />
+        </div>
+        <div className="lg:basis-[40%] lg:order-first">
+          <InfoContact />
+        </div>
+      </section>
+    </PageTransition>
   );
 }
--- src/app/(portfolio)/page.tsx
+++ src/app/(portfolio)/page.tsx
@@ -1,11 +1,12 @@
 import { Hero } from "./ui/Hero";
 import { Main } from "./ui/Main";
+import { PageTransition } from "@/components/page-transition/PageTransition";
 
 export default function HomePage() {
   return (
-    <>
+    <PageTransition>
       <Hero />
       <Main />
-    </>
+    </PageTransition>
   );
 }
